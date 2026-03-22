89fbcd374d3ba8762d3c88fb83a04627214a7ac7
Brendow Januzzi de Matos <brendowjanuzzi@gmail.com>
2026-03-22 16:44:37 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-// config.js
+// config.js - Copie e cole exatamente este conteúdo
+export const mapsKey = "AIzaSyB2frG9-iSSGQKeAsDUpyxUCSBdI0CCLp0";
+
 export const firebaseConfig = {
-  apiKey: "SUA_API_KEY_AQUI",
+  apiKey: "AIzaSyDqc9r2B1IDMyRb0hpZBWDo5XmYVp8D3g",
   authDomain: "alertavaga.firebaseapp.com",
   projectId: "alertavaga",
   storageBucket: "alertavaga.appspot.com",
   messagingSenderId: "1006667244458",
   appId: "1:1006667244458:web:4c2f35f5520843452c01d3"
 };
-
-export const mapsKey = "SUA_CHAVE_DO_MAPS_AQUI";
