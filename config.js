e430490c336e6453b995c3077ab9fee775b6adc0
Brendow Januzzi de Matos <brendowjanuzzi@gmail.com>
2026-03-22 16:34:10 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-// config.js - Configurações de API do Alerta-Vagas
+// config.js
 export const firebaseConfig = {
-  apiKey: "AIzaSyDqc9r2B1IDMyRb0hPZBWD3F43GlKZPzRQ",
-  authDomain: "alertavaga-56b46.firebaseapp.com",
-  projectId: "alertavaga-56b46",
-  storageBucket: "alertavaga-56b46.firebasestorage.app",
+  apiKey: "SUA_API_KEY_AQUI",
+  authDomain: "alertavaga.firebaseapp.com",
+  projectId: "alertavaga",
+  storageBucket: "alertavaga.appspot.com",
   messagingSenderId: "1006667244458",
   appId: "1:1006667244458:web:4c2f35f5520843452c01d3"
 };
 
-export const mapsKey = "AIzaSyB2frG9-iSSGQKeAsDUpyxUCSBdI0CCLp0";
+export const mapsKey = "SUA_CHAVE_DO_MAPS_AQUI";
